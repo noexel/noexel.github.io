62976bd9173f8c236a1656a20f27c3cdac7f6630
noexel <101858193+noexel@users.noreply.github.com>
2024-06-04 22:02:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let tg = window.Telegram.WebApp;
-secondary_bg_color = '#000000';
+
+tg.secondary_bg_color = '#000000';
 tg.expand();
 
+document.querySelector("#close-app").onclick = () => tg.impactOccurred('soft');
 document.querySelector("#close-app").onclick = () => tg.close();
+
