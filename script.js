8f1985f36819e30b3e22707c9725f20ec7aab0e2
noexel <101858193+noexel@users.noreply.github.com>
2024-06-05 19:17:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,10 @@ tg.setHeaderColor('#000');
 
 tg.expand();
 
-document.querySelector("#close-app").onclick = () => tg.HapticFeedback.selectionChanged();
 document.querySelector("#close-app").onclick = () => tg.close();
 
-document.querySelector(".go-beta").onclick = () => tg.HapticFeedback.selectionChanged();
 document.querySelector(".go-beta").onclick = () => location.href = 'form.html';
-document.querySelector(".go-beta").onclick = () => tg.BackButton.show();
+
 
 
 let mask = document.querySelector('.mask');
