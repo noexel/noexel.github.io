e9de2921fdc0bcd87024b4a3b82e978c05d93c56
noexel <101858193+noexel@users.noreply.github.com>
2024-06-05 17:56:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let tg = window.Telegram.WebApp;
 tg.setHeaderColor('#ffffff');
 
 tg.expand();
-document.querySelector("#close-app").onclick = () => tg.close(), tg.HapticFeedback.selectionChanged();
+document.querySelector("#close-app").onclick = () => tg.HapticFeedback.selectionChanged(), tg.close();
 document.querySelector(".go-beta").onclick = () => tg.HapticFeedback.selectionChanged();
 
 let mask = document.querySelector('.mask');
