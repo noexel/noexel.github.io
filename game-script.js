d0fb1546a259e57162ea20f06ec03fd7b4fae6fe
noexel <101858193+noexel@users.noreply.github.com>
2024-06-06 21:13:35 +0300

    Add files via upload

---
+++ game-script.js
@@ -0,0 +1,10 @@
+let tg2 = window.Telegram.WebApp;
+
+tg2.BackButton.hide();
+
+
+let game_mask = document.querySelector('.mask');
+
+window.addEventListener('load', () => {
+    game_mask.classList.add('hide')
+});
